9179974293220387c2cb7f086942926a1eda89ae
Anton Lyachek <lyachek.anton@gmail.com>
2019-12-13 23:34:10 +0500

    Modified parser to load more data

--- data_analysis/lab_3/parser.py
+++ data_analysis/lab_3/parser.py
@@ -1,18 +1,16 @@
 import requests
 import json
-1
-2
-4
-54
-95
-areas = ['1', '2', '4', '54', '95']
+# msc spb ekb nsk kazan'
+areas = ['1', '2', '3', '4', '88']
 search_url = 'https://api.hh.ru/vacancies?specialization=1&per_page=100&area='
 result = []
 
 for area in areas:
-	response = requests.get(search_url+area)
-	data = response.json()
-	result.extend(data['items'])
-    
+    for i in range(15):
+        response = requests.get(search_url+area+'&page='+str(i))
+        print('area '+area+'; page '+str(i))
+        data = response.json()
+        result.extend(data['items'])
+
 with open('data.json', 'w', encoding='utf-8') as json_file:
     json.dump(result, json_file, ensure_ascii=False)
